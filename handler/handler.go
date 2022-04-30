2349cb0a946493eae252cb93e235db7fa9f5dc94
CPZeam <1390757393@qq.com>
2022-04-30 15:46:50 +0800

    4.30-3

--- handler/handler.go
+++ handler/handler.go
@@ -1,19 +1,52 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"todomvc-app-template-golang/db"
+	"todomvc-app-template-golang/model"
+
+	"github.com/gin-gonic/gin"
+)
 
 func Add(c *gin.Context) {
-	
+	var p model.TodomvcAdd
+	c.ShouldBindJSON(&p)
+	var m = &model.Todomvc{Item: p.Item}
+	db.DB.Create(&m)
+	c.JSON(http.StatusOK, nil)
 }
 
 func Del(c *gin.Context) {
-
+	var p model.TodomvcDel
+	c.ShouldBindJSON(&p)
+	db.DB.Where("id", p.Id).Delete(&model.Todomvc{})
+	c.JSON(http.StatusOK, nil)
 }
 
 func Update(c *gin.Context) {
-
+	var p []model.TodomvcUpdate
+	c.ShouldBindJSON(&p)
+	for _, t := range p {
+		db.DB.Model(&model.Todomvc{}).Where("id", t.Id).Select("Name", "Age").Updates(model.Todomvc{
+			Item:   t.Item,
+			Status: t.Status,
+		})
+	}
+	c.JSON(http.StatusOK, nil)
 }
 
 func Find(c *gin.Context) {
+	var p model.TodomvcFind
+	c.ShouldBindJSON(&p)
+	var m []model.Todomvc
+	var tx = db.DB
+	if p.Status != -1 {
+		tx = tx.Where("status", p.Status)
+	}
+	if p.Item != "" {
+		tx = tx.Where("item LIKE?", "%"+p.Item+"p")
 
+	}
+	tx.Find(&m)
+	c.JSON(http.StatusOK, &m)
 }
--- model/model.go
+++ model/model.go
@@ -14,3 +14,22 @@ type ToDoMvc struct {
 	Item   string
 	Status uint
 }
+
+type TodomvcAdd struct {
+	Item string
+}
+
+type TodomvcDel struct {
+	Id uint
+}
+
+type TodomvcUpdate struct {
+	Item   string
+	Id     uint
+	Status uint
+}
+
+type TodomvcFind struct {
+	Item   string
+	Status uint
+}
